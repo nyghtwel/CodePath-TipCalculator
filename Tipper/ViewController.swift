c93e037a3a275ca652d61986871d2a4fd7fabe88
Alan Liou <nyghtwel@gmail.com>
2016-12-11 20:30:56 -0500

    first commit

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import SceneKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipSlider: UISlider!
+   
+    
+    
+    var currentValue:Int! = 5
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        billField.becomeFirstResponder()
+      
+
     }
 
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        print("tap working")
+
+        let touchLocation: CGPoint = sender.location(in: sender.view)
+        
+        
+        print(touchLocation)
+        
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        let percent = Double(currentValue)
+        
+        let tip = bill * percent / 100
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        currentValue! = Int(sender.value)
+        tipPercentLabel.text = "\(currentValue!)"+"%"
+        self.calculateTip(UITextField())
+    }
 
 }
 
